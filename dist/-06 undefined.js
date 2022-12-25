13348ce793cfae9e2a4fc7858eec0b3b7fede9ba
Abdulrahman Ahmed <abdulrahmanahmedrasheed@gmail.com>
2022-12-25 08:03:54 +0100

    undefine checking

--- dist/-06 undefined.js
+++ dist/-06 undefined.js
@@ -3,3 +3,13 @@ delete array[0];
 for (var i = 0; i < array.length; i++) {
     console.log("array[".concat(i, "] = ").concat(array[i]));
 }
+//
+for (var i = 0; i < array.length; i++) {
+    checkPrintElement(array[i]);
+}
+function checkPrintElement(arrElement) {
+    if (arrElement === undefined)
+        console.log("invali array element");
+    else
+        console.log("vallid array element: ".concat(arrElement));
+}
--- src/-06 undefined.ts
+++ src/-06 undefined.ts
@@ -6,3 +6,18 @@ for (let i = 0; i < array.length; i++){
     console.log(`array[${i}] = ${array[i]}`);
 
 }
+
+//
+
+for(let i = 0; i < array.length; i++){
+    checkPrintElement(array[i]);
+}
+
+function checkPrintElement(arrElement: string | undefined){
+    if(arrElement === undefined)
+        console.log(`invali array element`);
+    else
+    console.log(`vallid array element: ${arrElement}`);
+
+
+}
