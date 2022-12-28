b90d7acc0a3d499f76b4422c6a6451b79c59a298
Abdulrahman Ahmed <abdulrahmanahmedrasheed@gmail.com>
2022-12-28 08:22:32 +0100

    never switch

--- dist/-15 never.js
+++ dist/-15 never.js
@@ -2,3 +2,17 @@ function alwaysThrow() {
     throw new console.error("this always throw");
     //return -1;
 }
+// Never Switch
+var AnEnum;
+(function (AnEnum) {
+    AnEnum[AnEnum["FIRST"] = 0] = "FIRST";
+    AnEnum[AnEnum["SECOND"] = 1] = "SECOND";
+})(AnEnum || (AnEnum = {}));
+function getEnumValue(enumValue) {
+    switch (enumValue) {
+        case AnEnum.FIRST: return "FIRST CASE";
+        case AnEnum.SECOND: return "SECOND CASE";
+    }
+    var returnValue = enumValue;
+    return returnValue;
+}
--- src/-15 never.ts
+++ src/-15 never.ts
@@ -2,3 +2,19 @@ function alwaysThrow(): never{
     throw new console.error("this always throw");
     //return -1;
 }
+
+// Never Switch
+
+enum AnEnum {
+    FIRST,
+    SECOND
+}
+
+function getEnumValue(enumValue: AnEnum){
+    switch(enumValue){
+        case AnEnum.FIRST: return "FIRST CASE";
+        case AnEnum.SECOND: return "SECOND CASE";
+    }
+    let returnValue: never = enumValue;
+    return returnValue;
+}
