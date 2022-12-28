c526e93a2a5dbd9bfecadaf4ee5c436279eebd5d
Abdulrahman Ahmed <abdulrahmanahmedrasheed@gmail.com>
2022-12-28 04:43:38 +0100

    object

---
+++ dist/-13 object.js
@@ -0,0 +1,12 @@
+var structureObject = {
+    name: "nyObjeect",
+    properties: {
+        id: 1,
+        type: "AnObject"
+    }
+};
+function printObjectType(a) {
+    //console.log(`a: ${JSON.stringify(a)});
+}
+//printObjectType(structureObject)
+//printObjectType("this is a string");
---
+++ src/-13 object.ts
@@ -0,0 +1,14 @@
+let structureObject: object = {
+    name: "nyObjeect",
+    properties:{
+        id: 1,
+        type: "AnObject"
+    }
+}
+
+function printObjectType(a: object){
+    //console.log(`a: ${JSON.stringify(a)});
+}
+
+//printObjectType(structureObject)
+//printObjectType("this is a string");
