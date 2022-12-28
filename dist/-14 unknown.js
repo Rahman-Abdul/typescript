fdabc6a45def4ebdca458e0b6592509de07db44e
Abdulrahman Ahmed <abdulrahmanahmedrasheed@gmail.com>
2022-12-28 05:04:59 +0100

    unknown type

---
+++ dist/-14 unknown.js
@@ -0,0 +1,7 @@
+var a = "test";
+var aNumber = 2;
+aNumber = a;
+// Unknown type: The different between unknown type and any type is that unknown type cannot be assign to other tyoe without explicit casting.
+var b = "test";
+var bNumber = 2;
+//aNumber = b;
---
+++ src/-14 unknown.ts
@@ -0,0 +1,9 @@
+let a: any = "test";
+let aNumber : number = 2;
+aNumber = a;
+
+// Unknown type: The different between unknown type and any type is that unknown type cannot be assign to other tyoe without explicit casting.
+
+let b: unknown = "test";
+let bNumber : number = 2;
+//aNumber = b;
