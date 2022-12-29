c140ee293f0dedff90260e8a05467b1c0773fd7c
Abdulrahman Ahmed <abdulrahmanahmedrasheed@gmail.com>
2022-12-29 07:38:58 +0100

    spread with array

---
+++ src/-16 array.ts
@@ -0,0 +1,12 @@
+let array1 = ["hello", 1, true, "null"];
+
+// Spread with arrays
+
+let firstArray = [1, 2, 3];
+let secondArray = [4, 5, 6];
+let thirdArray = [...firstArray, ...secondArray]
+
+console.log(`third array = ${thirdArray}`);
+
+
+
