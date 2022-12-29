05862d99037bcd54207a0f32f3b0887cdd8c751d
Abdulrahman Ahmed <abdulrahmanahmedrasheed@gmail.com>
2022-12-29 07:48:38 +0100

    array with spread

--- src/-16 array.ts
+++ src/-16 array.ts
@@ -8,5 +8,30 @@ let thirdArray = [...firstArray, ...secondArray]
 
 console.log(`third array = ${thirdArray}`);
 
+// spread oject with array
 
+let objArray1 = [
+    {
+        id: 1,
+        name: "first element"
+    }
+]
+
+let objArray2 = [
+    {
+        id: 2,
+        name: "second element"
+    }
+]
+
+let objArray3 = [
+    ...objArray1,
+    {
+        id: 3,
+        name: "third element"
+    },
+    ...objArray2
+]
+
+console.log(`objArray3 = ${JSON.stringify(objArray3, null, 4)}`);
 
