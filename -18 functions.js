6b27f5fd486969415247b365d4f0356b2642ebbe
Abdulrahman Ahmed <abdulrahmanahmedrasheed@gmail.com>
2022-12-29 15:18:17 +0100

    functions in typescript

--- -18 functions.js
+++ -18 functions.js
@@ -8,3 +8,32 @@ function concatValues(a, b) {
 }
 concatValues("first", "second");
 concatValues("third");
+// Default parameters
+function concatWithDefault(a, b) {
+    if (b === void 0) { b = "default"; }
+    console.log("a + b = ".concat(a + b));
+}
+concatWithDefault("first", "second");
+concatWithDefault("third");
+//Rest parameters
+/*
+function testArgument(){
+    for (var i = 0; i < arguments.length; i++){
+        console.log("argument[" + i + "] =" + arguments[i]);
+
+    }
+}
+*/
+//testArgument(1, 2);
+//testArgument("first", "second", "third");
+function testArgument() {
+    var args = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        args[_i] = arguments[_i];
+    }
+    for (var i in args) {
+        console.log("args[".concat(i, "] = ").concat(args[i]));
+    }
+}
+testArgument(1, 2);
+testArgument(10, 20);
--- -18 functions.ts
+++ -18 functions.ts
@@ -13,3 +13,36 @@ function concatValues(a: string, b?: string){
 
 concatValues("first", "second");
 concatValues("third");
+
+// Default parameters
+
+function concatWithDefault(a:string, b: string = "default") {
+    console.log(`a + b = ${a + b}`);
+
+
+}
+
+concatWithDefault("first", "second");
+concatWithDefault("third");
+
+//Rest parameters
+/*
+function testArgument(){
+    for (var i = 0; i < arguments.length; i++){
+        console.log("argument[" + i + "] =" + arguments[i]);
+
+    }
+}
+*/
+//testArgument(1, 2);
+
+//testArgument("first", "second", "third");
+
+function testArgument(...args: string[] | number[]){
+    for (let i in args){
+        console.log(`args[${i}] = ${args[i]}`);
+
+    }
+}
+testArgument(1, 2)
+testArgument(10, 20)
