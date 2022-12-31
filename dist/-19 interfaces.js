74364847d21f0a903f668e6ab8e74b93ca246763
Abdulrahman Ahmed <abdulrahmanahmedrasheed@gmail.com>
2022-12-31 08:00:54 +0100

    interface

---
+++ dist/-19 interfaces.js
@@ -0,0 +1,5 @@
+// Object containing interface
+var idobject = {
+    id: 2,
+    name: "this is a nme"
+};
---
+++ src/-19 interfaces.ts
@@ -0,0 +1,12 @@
+interface IIdName {
+    id: number;
+    name: string;
+}
+
+// Object containing interface
+
+let idobject: IIdName = {
+    id: 2,
+    name: "this is a nme"
+}
+
