aa13caf4bcab0f65c40fce892eda4950a0a95533
Abdulrahman Ahmed <abdulrahmanahmedrasheed@gmail.com>
2023-01-01 09:19:34 +0100

    classes

--- dist/-20 classes.js
+++ dist/-20 classes.js
@@ -9,3 +9,28 @@ var SimpleClass = /** @class */ (function () {
 var mySimpleClass = new SimpleClass();
 mySimpleClass.id = 2020;
 mySimpleClass.print();
+function printClass(a) {
+    a.print();
+}
+var ClassA = /** @class */ (function () {
+    function ClassA() {
+    }
+    ClassA.prototype.print = function () {
+        console.log("ClassA.print called");
+    };
+    ;
+    return ClassA;
+}());
+var ClassB = /** @class */ (function () {
+    function ClassB() {
+    }
+    ClassB.prototype.print = function () {
+        console.log("ClassB.print() called");
+    };
+    ;
+    return ClassB;
+}());
+var classA = new ClassA();
+var classB = new ClassB();
+printClass(classA);
+printClass(classB);
--- src/-20 classes.ts
+++ src/-20 classes.ts
@@ -9,3 +9,30 @@ let mySimpleClass = new SimpleClass();
 mySimpleClass.id = 2020;
 mySimpleClass.print()
 
+// Implementing Interface
+interface Iprint {
+    print(): void;
+}
+
+function printClass(a: Iprint){
+    a.print()
+}
+class ClassA implements Iprint {
+    print(): void {
+        console.log(`ClassA.print called`);
+
+    };
+}
+
+class ClassB implements Iprint {
+    print(): void {
+        console.log(`ClassB.print() called`);
+
+    };
+}
+
+let classA = new ClassA();
+let classB = new ClassB();
+
+printClass(classA);
+printClass(classB);
